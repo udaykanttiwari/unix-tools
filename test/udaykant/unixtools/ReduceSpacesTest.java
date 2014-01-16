e439f13f192811767554893dd8a33d195cee2954
udaykant <udaykant@thoughtworks.com>
2014-01-16 10:40:27 +0530

    reduce space test is renamed

--- test/udaykant/unixtools/ReduceSpacesTest.java
+++ test/udaykant/unixtools/ReduceSpacesTest.java
@@ -2,18 +2,18 @@ package udaykant.unixtools;
 
 import org.junit.Assert;
 
-public class ReduceSpaceTest {
+public class ReduceSpacesTest {
     @org.junit.Test
     public void testReduceSpace_for_single_line() throws Exception {
         String expected = "uday kant tiwari";
-        ReduceSpace rs = new ReduceSpace();
+        ReduceSpaces rs = new ReduceSpaces();
         String actual= rs.reduceSpace("uday     kant    tiwari");
         Assert.assertEquals(expected,actual);
     }
     @org.junit.Test
     public void testReduceSpace_for_multiline_content() throws Exception {
         String expected = "uday kant tiwari\nharjas singh";
-        ReduceSpace rs = new ReduceSpace();
+        ReduceSpaces rs = new ReduceSpaces();
         String actual= rs.reduceSpace("uday     kant    tiwari\nharjas      singh");
         Assert.assertEquals(expected,actual);
     }
